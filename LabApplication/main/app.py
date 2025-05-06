2a44a2e21a58a0fa1ceeca4862e8fdffc8452060
Henry Rothenberg <hsr205@aol.com>
2025-05-06 10:57:42 -0400

    Add lab logic

--- LabApplication/main/app.py
+++ LabApplication/main/app.py
@@ -2,27 +2,47 @@ import random
 
 from kivy.app import App
 from kivy.metrics import sp
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.switch import Switch
+from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.widget import Widget
 
 
 class LabCounterGridLayout(GridLayout):
     counter_int: NumericProperty = NumericProperty(1)
     random_float: NumericProperty = NumericProperty(1.0)
+    state_str: StringProperty = StringProperty("OFF")
+    enable_button: BooleanProperty = BooleanProperty(True)
+    enable_slider: BooleanProperty = BooleanProperty(True)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def increment_counter(self) -> None:
-        self.counter_int += 1
+        if self.enable_button is False:
+            self.counter_int += 1
 
     def get_random_float_value(self) -> None:
         self.random_float = random.random()
+
+    def execute_toggle_button_behaviour(self, toggle_button: ToggleButton) -> None:
+        if toggle_button.state == "down":
+            self.state_str = "ON"
+            self.enable_button = False
+        else:
+            self.state_str = "OFF"
+            self.enable_button = True
+
+    def on_switch_active(self, switch_widget: Switch) -> None:
+        if switch_widget.active:
+            self.enable_slider = False
+        else:
+            self.enable_slider = True
 
 
 class LabStackLayout(StackLayout):
